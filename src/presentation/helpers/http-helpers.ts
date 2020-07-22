e8fd32f972de43e3470ef7e5f26219282b2c7178
Vinicius <amorimhavoc@gmail.com>
2020-07-21 22:47:26 -0300

    refactor

--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -1,5 +1,5 @@
-import { HttppResponse } from './../protocols/http'
-export const badRequest = (error: Error): HttppResponse => ({
+import { HttpResponse } from './../protocols/http'
+export const badRequest = (error: Error): HttpResponse => ({
 
   statusCode: 400,
   body: error
