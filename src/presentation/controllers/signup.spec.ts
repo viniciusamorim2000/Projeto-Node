9c5c0b9e8a932801ac6df3d7b39472b3a6267850
Vinicius <amorimhavoc@gmail.com>
2020-07-21 21:45:35 -0300

    test: ensure SignUpController return 400

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup'
+describe('Signup Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'Vinicius ',
+        email: 'amorimhavoc@gmail.com',
+        passoword: 'any_passoword',
+        passowordConfirmation: 'any_confirmation'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    sut.handle(httpRequest)
+  })
+})
