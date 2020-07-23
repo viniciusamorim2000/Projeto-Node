3916bc6190c0c9c55334270e452b48078defe347
Vinicius <amorimhavoc@gmail.com>
2020-07-23 12:17:04 -0300

    refactor move sut creation to a feactory method

--- utils/email-validator-adapter.spec.ts
+++ utils/email-validator-adapter.spec.ts
@@ -6,17 +6,20 @@ jest.mock('validator', () => ({
     return true
   }
 }))
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
 
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid@mail.com')
     expect(isValid).toBe(false)
   })
 
   test('Should return true if validator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('valid@mail.com')
     expect(isValid).toBe(true)
   })
