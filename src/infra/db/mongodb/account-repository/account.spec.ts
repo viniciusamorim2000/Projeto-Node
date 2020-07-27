6afc7a4380b0ec7d1e5bbefcd56e4a0134ac92e8
Vinicius <amorimhavoc@gmail.com>
2020-07-27 10:21:53 -0300

    test: ensure AccountMongoRepository returns an account on sucess

---
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,26 @@
+import { AccountMongoRepository } from './account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+describe('Account Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Shoul return an account on sucess', async () => {
+    const sut = new AccountMongoRepository()
+    const account = await sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      passoword: 'any_passoword'
+    })
+    expect(account).toBeTruthy()
+    expect(account.id).toBeTruthy()
+    expect(account.name).toBe('any_name')
+    expect(account.email).toBe('any_email@mail.com')
+    expect(account.passoword).toBe('any_passoword')
+  })
+})
