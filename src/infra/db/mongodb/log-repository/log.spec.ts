8991d55b8e2543c4f965b7827679ac77ffe3b8ef
Vinicius <amorimhavoc@gmail.com>
2020-07-30 09:00:36 -0300

    refactor: move to a sut method

--- src/infra/db/mongodb/log-repository/log.spec.ts
+++ src/infra/db/mongodb/log-repository/log.spec.ts
@@ -2,6 +2,9 @@ import { LogMongoRepository } from './log'
 import { MongoHelper } from './../helpers/mongo-helper'
 import { Collection } from 'mongodb'
 
+const makeSut = (): LogMongoRepository => {
+  return new LogMongoRepository()
+}
 describe('Log Mongo Repository', () => {
   let errorCollection: Collection
 
@@ -19,7 +22,7 @@ describe('Log Mongo Repository', () => {
   })
 
   test('Should create an log on sucess', async () => {
-    const sut = new LogMongoRepository()
+    const sut = makeSut()
     await sut.logError('any_error')
     const count = await errorCollection.countDocuments()
     expect(count).toBe(1)
